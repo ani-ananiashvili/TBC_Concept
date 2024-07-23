ac1a0198a816ace3e97336b703be3f4b5e9f4bc6
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-07-23 17:46:15 +0400

    edit JS

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,6 @@
 let currentSlide = 0;
+let isDragging = false;
+let startX, initialOffsetX;
 
 function showSlide(index) {
     const slides = document.querySelectorAll('.slide');
@@ -14,12 +16,52 @@ function showSlide(index) {
     document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
 
     const lineOffset = currentSlide * (100 / totalSlides);
-    document.querySelector('.line').style.transform = `translateX(${lineOffset}%)`;
+    document.querySelector('.progress-line').style.transform = `translateX(${lineOffset}%)`;
 }
 
 function moveSlide(n) {
     showSlide(currentSlide + n);
 }
 
+function onMouseDown(e) {
+    isDragging = true;
+    startX = e.clientX || e.touches[0].clientX;
+    const progressLine = document.querySelector('.progress-line');
+    initialOffsetX = progressLine.getBoundingClientRect().left;
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('touchmove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+    document.addEventListener('touchend', onMouseUp);
+}
+
+function onMouseMove(e) {
+    if (!isDragging) return;
+    const x = e.clientX || e.touches[0].clientX;
+    const dx = x - startX;
+    const progressLine = document.querySelector('.progress-line');
+    const lineContainer = document.querySelector('.line-container');
+    const maxOffset = lineContainer.clientWidth - progressLine.clientWidth;
+    let newOffsetX = initialOffsetX + dx - lineContainer.getBoundingClientRect().left;
+
+    newOffsetX = Math.max(0, Math.min(newOffsetX, maxOffset));
+
+    progressLine.style.transform = `translateX(${newOffsetX}px)`;
+
+    const newSlide = Math.round((newOffsetX / maxOffset) * (document.querySelectorAll('.slide').length / 3 - 1));
+    showSlide(newSlide);
+}
+
+function onMouseUp() {
+    isDragging = false;
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('touchmove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+    document.removeEventListener('touchend', onMouseUp);
+}
+
 // Initial display
 showSlide(currentSlide);
+
+const progressLine = document.querySelector('.progress-line');
+progressLine.addEventListener('mousedown', onMouseDown);
+progressLine.addEventListener('touchstart', onMouseDown);
